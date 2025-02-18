2cf4e4398c793fa7f9be3b3144a1105068452c36
Arturo Cuicas <arturo.cuicas@gmail.com>
2025-02-18 16:55:02 -0600

    Add one more problem solved

--- problems/Contains Duplicate/python/main.py
+++ problems/Contains Duplicate/python/main.py
@@ -3,7 +3,14 @@ from typing import List
 
 class Solution:
     def contains_duplicate(self, nums: List[int]) -> bool:
-        return len(nums) > len(set(nums))
+        # return len(nums) > len(set(nums))
+        nums.sort()
+
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+
+        return False
 
 solution = Solution()
 print(solution.contains_duplicate([1,2,3,1]))
